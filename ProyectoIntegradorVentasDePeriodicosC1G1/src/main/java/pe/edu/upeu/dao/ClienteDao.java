82893fbb1d6e5f5c40d623a512169c005274b3be
JosueGabriel0 <josue.ochoa@upeu.edu.pe>
2021-12-20 16:51:03 -0500

    QuioscoDePeriodicos

--- ProyectoIntegradorVentasDePeriodicosC1G1/src/main/java/pe/edu/upeu/dao/ClienteDao.java
+++ ProyectoIntegradorVentasDePeriodicosC1G1/src/main/java/pe/edu/upeu/dao/ClienteDao.java
@@ -7,6 +7,28 @@ import pe.edu.upeu.utils.LeerTeclado;
 import pe.edu.upeu.utils.UtilsX;
 
 public class ClienteDao extends AppCrud{
+
+     //Reset
+     public static final String ANSI_RESET = "\u001B[0m";
+     //Colores de letra
+     public static final String ANSI_BLACK = "\u001B[30m";
+     public static final String ANSI_RED = "\u001B[31m";
+     public static final String ANSI_GREEN = "\u001B[32m";
+     public static final String ANSI_YELLOW = "\u001B[33m";
+     public static final String ANSI_BLUE = "\u001B[34m";
+     public static final String ANSI_PURPLE = "\u001B[35m";
+     public static final String ANSI_CYAN = "\u001B[36m";
+     public static final String ANSI_WHITE = "\u001B[37m";
+     //Colores de fondo
+     public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+     public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+     public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+     public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+     public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+     public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
     //Objetos 
     LeerTeclado leerTecla=new LeerTeclado();
     UtilsX util=new UtilsX();
@@ -22,8 +44,8 @@ public class ClienteDao extends AppCrud{
         cTo=new ClienteTO();        
         if(validarDni(dni)){
             cTo.setDni(dni);
-            cTo.setNombre(leerTecla.leer("", "Ingrese su nombre"));
-            cTo.setCelular(leerTecla.leer("", "Ingrese su Celular"));
+            cTo.setNombre(leerTecla.leer("", ANSI_CYAN+"Ingrese su nombre"+ANSI_RESET));
+            cTo.setCelular(leerTecla.leer("", ANSI_CYAN+"Ingrese su Celular"+ANSI_RESET));
             leerArch=new LeerArchivo(TABLA_CLIENTE);
             agregarContenido(leerArch, cTo);
         }
